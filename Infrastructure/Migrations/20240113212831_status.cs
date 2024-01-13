a09d4e48dc636bc5b6bb8fc3e18d137665b0ad09
Lenovo <tareqewida@gmail.ocm>
2024-01-14 00:51:41 +0200

    ..

---
+++ Infrastructure/Migrations/20240113212831_status.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class status : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Suppliers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "shoppingCarts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "payments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "employees",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Customers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Categories",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "branches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "shoppingCarts");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "payments");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "employees");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "branches");
+        }
+    }
+}
